805d3f5c553c857ef00b0898a33450872929a83d
Brandd_dev <98653274+UnderWorksDigi@users.noreply.github.com>
2025-09-09 20:03:53 +0100

    Rewrite project to CRA format (remove Vite, add CRA index.html + index.js)

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "dist")));
+// Serve React build
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
