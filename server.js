00d8e5d5b266f0aa0e27a0ed61dcc58467b70307
Brandd_dev <98653274+UnderWorksDigi@users.noreply.github.com>
2025-09-09 19:52:11 +0100

    Rewrite server to CommonJS for Railway compatibility

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-// server.js
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from dist/
 app.use(express.static(path.join(__dirname, "dist")));
 
-// For any route, send index.html (SPA support)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
